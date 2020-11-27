6d2306612f12c2ebd1bf9010ddfd8c754980f9e4
kazekageyoga <kazekageyoga60@yahoo.co.id>
2020-11-27 17:52:52 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, ut! Rem accusantium repudiandae obcaecati eos possimus nisi eveniet cumque eum. Ratione modi consectetur dolorem facere, ipsum consequatur blanditiis inventore nulla.</p>
 </body>
 </html>
